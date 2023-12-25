e28b397b2b785b516662e9ca89cb8277e253cfd6
nocturneplayer <omegapatay@yandex.ru>
2023-12-25 09:12:37 +0300

    Добавьте файлы проекта.

---
+++ Kursovaya/FileName.c
@@ -0,0 +1,208 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+#include <string.h>
+#include <malloc.h>
+#define _USE_MATH_DEFINES
+
+struct Incident
+{
+	char datatime[20];
+	char discrip[50];
+	char category[50];
+	int priority;
+	short status;
+	char respons[30];
+};
+
+int StructReading(struct Incident* incident, int inc_count);
+int FileWriting(struct Incident* incident, int inc_count);
+int MakeIncidentInfo(struct Incident* incident);
+int FindRespons(struct Incident* incident, int inc_count);
+int FindCategory(struct Incident* incident, int inc_count);
+int MassiveSort(struct Incident* arr, int inc_count);
+
+//Основная функция
+void main()
+{
+	FILE* file;
+	system("chcp 1251");
+	setlocale(LC_ALL, "RUS");
+	struct Incident incident[100];
+	int inc_count = 0;
+	int ans1;
+	do
+	{
+		printf("---------------------------------------------------------------------------------------\n");
+		puts("Выберите действие:");
+		puts("1) Создать запись об инциденте.");
+		puts("2) Вывести записанные в буфер инциденты.");
+		puts("3) Отсортировать по приоритету (Высокий -> Низкий).");
+		puts("4) Записать введённые инфиденты в файл.");
+		puts("5) Найти инцидент(-ы) по ответственному.");
+		puts("6) Найти инцидент(-ы) по категории.");
+		puts("7) Очистить файл.");
+		puts("8) Завершить работу программы.");
+		printf("---------------------------------------------------------------------------------------\n");
+		scanf("%d", &ans1);
+		getchar();
+		printf("---------------------------------------------------------------------------------------\n");
+		switch (ans1)
+		{
+		case 1:
+			if (inc_count < 100)
+			{
+				struct Incident newincident;
+				MakeIncidentInfo(&newincident);
+				incident[inc_count] = newincident;
+				inc_count++;
+			}
+			else
+			{
+				printf("Слишком много записей");
+			}
+			break;
+		case 2:
+			if (inc_count > 0)
+			{
+				StructReading(incident, inc_count);
+			}
+			else
+			{
+				printf("Записанные инциденты отсутствуют\n");
+			}
+			break;
+		case 3:
+			MassiveSort(incident, inc_count);
+			printf("Инциденты отсортированы\n");
+			for (int i = 0; i < inc_count; i++)
+			{
+				FileWriting(incident, inc_count);
+			}
+			break;
+		case 4:
+			FileWriting(incident, inc_count);
+			puts("Информация об инцидентах записана в файл.");
+			break;
+		case 5:
+			printf("Введите имя необходимого ответственного: ");
+			FindRespons(incident, inc_count);
+			break;
+		case 6:
+			printf("Введите необходимую категорию: ");
+			FindCategory(incident, inc_count);
+			break;
+		case 7:
+			file = fopen("data.txt", "w");
+			inc_count = 0;
+			printf("Файл очищен\n");
+			break;
+		default:
+			if (ans1 != 8)
+			{
+				printf("Неправильное значение\n");
+			}
+			break;
+		}
+	} while (ans1 != 8);
+}
+
+//Функция записи в файл
+int FileWriting(struct Incident* incident, int inc_count)
+{
+	FILE* file = fopen("data.txt", "w");
+	if (file == NULL)
+	{
+		printf("Файл не существуют");
+		return;
+	}
+	for (int i = 0; i < inc_count; ++i)
+	{
+		fprintf(file, "%s%s%s%d\n%i\n%s\n", incident[i].datatime, incident[i].discrip, incident[i].category, incident[i].priority, incident[i].status, incident[i].respons);
+	}
+	fclose(file);
+	return 1;
+}
+
+//Функция создания инцидента
+int MakeIncidentInfo(struct Incident* incident)
+{
+	printf("Введите дату и время инцидента (dd.mm.yyy/hh:mm:ss): ");
+	fgets(incident->datatime, sizeof(incident->datatime), stdin);
+	printf("Введите описание инцидента: ");
+	fgets(incident->discrip, sizeof(incident->discrip), stdin);
+	printf("Введите категорию инцидента (Уведомление о компьютерном инциденте/Уведомление о компьютерной атаке/Уведомление о наличии уязвимости): ");
+	fgets(incident->category, sizeof(incident->category), stdin);
+	printf("Введите приоритет инцидента (1 - Низкий, 2 - Средний, 3 - Высокий): ");
+	scanf("%d", &incident->priority);
+	getchar();
+	printf("Введите статус инцидента (1 - На рассмотрении, 2 - Рассмотрено, 3 - Приняты меры): ");
+	scanf("%d", &incident->status);
+	getchar();
+	printf("Введите ответственного: ");
+	fgets(incident->respons, sizeof(incident->respons), stdin);
+	printf("--------------------------------------------------------------------------------\n");
+	return 1;
+}
+
+//Функция вывода в консоль инцидентов
+int StructReading(struct Incident* incident, int inc_count)
+{
+	for (int i = 0; i < inc_count; i++)
+	{
+		printf("%s%s%s%d\n%i\n%s\n", incident[i].datatime, incident[i].discrip, incident[i].category, incident[i].priority, incident[i].status, incident[i].respons);
+	}
+
+	return 1;
+}
+
+//Функция поиска инцидента по ответственному
+int FindRespons(struct Incident* incident, int inc_count)
+{
+	char res[30];
+	fgets(res, sizeof(res), stdin);
+	for (int i = 0; i < inc_count; i++)
+	{
+		if (strcmp(incident[i].respons, res) == 0)
+		{
+			printf("%s%s%s%d\n%i\n%s\n", incident[i].datatime, incident[i].discrip, incident[i].category, incident[i].priority, incident[i].status, incident[i].respons);
+		}
+	}
+	return 1;
+}
+
+//Функция поиска инцидента по категории
+int FindCategory(struct Incident* incident, int inc_count)
+{
+	char cat[50];
+	fgets(cat, sizeof(cat), stdin);
+	for (int i = 0; i < inc_count; i++)
+	{
+		if (strcmp(incident[i].category, cat) == 0)
+		{
+			printf("%s%s%s%d\n%i\n%s\n", incident[i].datatime, incident[i].discrip, incident[i].category, incident[i].priority, incident[i].status, incident[i].respons);
+		}
+	}
+	return 1;
+}
+
+//Сортировка массива по приоритету (Высокий -> Низкий)
+int MassiveSort(struct Incident* arr, int inc_count)
+{
+	struct Incident temp;
+	for (int i = 0; i < inc_count - 1; i++)
+	{
+		for (int j = 0; j < inc_count - i - 1; j++)
+		{
+			if (arr[j].priority < arr[j + 1].priority)
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return 1;
+}
